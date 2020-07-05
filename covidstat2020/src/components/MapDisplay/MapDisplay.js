47b17ac821ca8997df70f9b8ce763b94774fff00
devina dhuri <ddhuri1@binghamton.edu>
2020-07-05 19:54:59 -0400

    Final data fetch

--- covidstat2020/src/components/MapDisplay/MapDisplay.js
+++ covidstat2020/src/components/MapDisplay/MapDisplay.js
@@ -117,7 +117,62 @@ function MapDisplay() {
 
         let lastId;
 
+        map.on("mousemove", "circles", e => {
+          const id = e.features[0].properties.id;
 
+          if (id !== lastId) {
+            lastId = id;
+            const {
+              cases,
+              deaths,
+              country,
+              province
+            } = e.features[0].properties;
+
+            // Change the pointer type on mouseenter
+            map.getCanvas().style.cursor = "pointer";
+
+            const coordinates = e.features[0].geometry.coordinates.slice();
+
+            const countryISO =
+              lookup.byCountry(country)?.iso2 ||
+              lookup.byInternet(country)?.iso2;
+            const provinceHTML =
+              province !== "null" ? `<p>Province: <b>${province}</b></p>` : "";
+            const mortalityRate = ((deaths / cases) * 100).toFixed(2);
+            const countryFlagHTML = Boolean(countryISO)
+              ? `<img src="https://www.countryflags.io/${countryISO}/flat/64.png"></img>`
+              : "";
+
+            const HTML = `<p>Country: <b>${country}</b></p>
+                ${provinceHTML}
+                <p>Cases: <b>${cases}</b></p>
+                <p>Deaths: <b>${deaths}</b></p>
+                <p>Mortality Rate: <b>${mortalityRate}%</b></p>
+                ${countryFlagHTML}`;
+
+            // Ensure that if the map is zoomed out such that multiple
+            // copies of the feature are visible, the popup appears
+            // over the copy being pointed to.
+            while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+              coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+            }
+
+            popup
+              .setLngLat(coordinates)
+              .setHTML(HTML)
+              .addTo(map);
+          }
+        });
+
+        map.on("mouseleave", "circles", function() {
+          lastId = undefined;
+          map.getCanvas().style.cursor = "";
+          popup.remove();
+        });
+      });
+    }
+  }, [data]);
       
   return (
         <div ref={mapboxElRef } className="mapWrapper" />
